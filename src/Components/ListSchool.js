cf35ecb76cf0d6b93578df9f73f923b37e5e37de
Suresh Chaudhari <codebrust@gmail.com>
2020-08-08 14:03:45 +0545

    list school modified

--- src/Components/ListSchool.js
+++ src/Components/ListSchool.js
@@ -15,32 +15,42 @@ class ListStudent extends Component{
   
     constructor(props){
         super(props);
-        // console.log(this.props)
+        console.log(this.props)
         this.state = {
-            load: false
-        }
+            load: false,
+            sdata: []
+        };
+        this.getSchools = this.getSchools.bind(this)
     }
 
     getSchools(params) {
         axios.get('http://localhost:3008/api/school', {
             headers: {
-              auth:""
+              authtoken:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Ik5hbWUiOm51bGwsIklEIjoyLCJVc2VybmFtZSI6InRlYWNoZXIiLCJVc2VydHlwZUlEIjo0LCJVc2VydHlwZSI6IlRlYWNoZXIiLCJTY2hvb2xJRCI6MX0sImlhdCI6MTU5Njc4Mzk0NSwiZXhwIjoxNTk2ODcwMzQ1fQ.MrAMebr5N3_FF3j2cDPOx5xKzA4bwcFvTxMBTzOOqGI"
             }
           })
-          .then(function (response) {
+          .then((response)=>{
               Schools = response.data;
-              this.setState({load :true});
+              this.setState({
+                  load : true
+              })
             console.log(Schools);
           })
           .catch(function (error) {
-            // console.log({error});
+            console.log({error});
           })
         
     }
     
     render(){
         // console.log(this.props.persons);
-        this.getSchools();
+        {if(!this.state.load){
+            this.getSchools()
+            }
+            else{
+                console.log(this.state.load)
+             }
+            }
         return(
         //     Schools.map((school)=>(
         //     <SchoolItem key={school.SchoolID} school = {school}/>
@@ -54,7 +64,13 @@ class ListStudent extends Component{
           </Tr>
         </Thead>
         <Tbody>
-          <Tr>
+            {Schools.map((school)=>(
+                <Tr>
+                    <Td>{school.Name}</Td>
+                </Tr>
+             ))}
+
+          {/* <Tr>
             <Td>Tablescon</Td>
             <Td>9 April 2019</Td>
             <Td>East Annex</Td>
@@ -68,7 +84,7 @@ class ListStudent extends Component{
             <Td>Tuscaloosa D3</Td>
             <Td>29 June 2019</Td>
             <Td>Github</Td>
-          </Tr>
+          </Tr> */}
         </Tbody>
       </Table>
         )
